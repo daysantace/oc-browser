2372d18a5a147a9e9fb8d5da2fb950666f3810e6
daysantace <daysantace@gmail.com>
2023-02-24 21:59:21 +0000

    home menu fix + some other options

--- src/browser.lua
+++ src/browser.lua
@@ -1,38 +1,111 @@
--- config
-local port = 25565
+--config
+port = 25565
+dnsserver = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"-- address goes here
 
--- import libs and initialise
-local component = require("component")
-local event = require("event")
+--import libs and initialise
+component = require("component")
+event = require("event")
 
-local modem = component.modem
-local gpu = component.gpu
-local colors = require("colors")
+modem = component.modem
+gpu = component.gpu
+colors = require("colors")
+keyboard = require("keyboard")
+term = require("term")
 
 gpu.setResolution(80,25)
-gpu.setDepth(2)
+pagetitle = "OC-Browser Home Screen"
+pageaddress = "ocbrowser"
+term.setCursorBlink(false)
 
--- top bar
-function topbar()
-gpu.setBackground(colors.blue)
-gpu.setForeground(colors.white)
-gpu.fill(1,1,80,2," ")
-
-gpu.set(1,1," " .. pagetitle)
-gpu.set(1,2," " .. address)
-
-gpu.set(70,2," ")
-gpu.set(80,2," ")
-gpu.setBackground(colors.gray)
-gpu.setForeground(colors.silver)
-gpu.fill(71,2,79,2," ")
-gpu.set(72,2,"Search")
-
-gpu.setBackground(colors.red)
-gpu.setForeground(colors.white)
-gpu.set(80,1,"X")
-end
 -- welcome screen
-local pagetitle = "OC-Browser Welcome Screen"
-local address = " "
-topbar()
+::labelhome::
+
+term.clear()
+gpu.setBackground(colors.blue,true)
+gpu.setForeground(colors.white,true)
+gpu.fill(1,1,80,2," ")
+gpu.set(1,1," " .. pagetitle)
+gpu.set(1,2," " .. pageaddress)
+gpu.setBackground(colors.black,true)
+term.setCursor(1,3)
+print(" ")
+print(" Welcome to OC-Browser.")
+print(" ")
+print(" Ctrl + L - Search")
+print(" Ctrl + Alt + C - Exit")
+print(" Ctrl + R - Refresh")
+print(" Ctrl + N - Home page")
+gpu.setForeground(colors.gray,true)
+gpu.set(2,24,"OC-Browser 1.0 / Made by daysant")
+gpu.setForeground(colors.white,true)
+
+::labelinput::
+
+-- get user input
+if keyboard.isControlDown() then
+    if keyboard.isKeyDown("H") then
+        goto labelhome
+    end
+    if keyboard.isKeyDown("W") then
+        gpu.fill(1, 1, 80, 25, " ")
+        os.exit()
+    end
+    if keyboard.isKeyDown("R") then
+        goto labelloadpage
+    end
+    if keyboard.isKeyDown("L") then
+        goto labelsearch
+    end
+end
+
+os.sleep(0.1)
+
+goto labelinput
+-- search
+:: labelsearch ::
+
+term.clear()
+gpu.setBackground(colors.blue,true)
+gpu.setForeground(colors.white,true)
+gpu.fill(1,1,80,2," ")
+gpu.set(1,1," Search")
+gpu.set(1,2," ocbrowser")
+gpu.setBackground(colors.black,true)
+term.setCursor(1,3)
+print(" ")
+print(" Search")
+print(" ")
+search = io.read()
+
+-- get server address
+modem.open(port)
+modem.send(dnsserver,port,"search-" .. search)
+_,_,_,_,_,serveraddress = event.pull("modem_message")
+modem.close(port)
+
+:: labelloadpage ::
+
+-- if server address is not found
+if serveraddress == "error-not-found" then
+    term.clear()
+    gpu.setBackground(colors.blue,true)
+    gpu.setForeground(colors.white,true)
+    gpu.fill(1,1,80,2," ")
+    gpu.set(1,1," Error - Domain not found")
+    gpu.set(1,2," ocbrowser")
+    gpu.setBackground(colors.black,true)
+    term.setCursor(1,3)
+    print(" ")
+    print(" The server address could not be found.")
+    print(" Ensure you typed in the address correctly.")
+    print(" If it does not work, the server may be down or have moved.")
+else
+    -- request page content
+    modem.open(port)
+    modem.send(serveraddress,port,"req-content")
+    _,_,_,_,_,pagecontent = event.pull("modem_message")
+    modem.close(port)
+    -- process content
+end
+
+goto labelinput
