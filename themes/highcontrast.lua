2ef546eaaae96106a7a861bf462834c2c3d49d4c
daysant <92995911+daysantace@users.noreply.github.com>
2023-02-28 12:41:28 +0000

    high contrast

---
+++ themes/highcontrast.lua
@@ -0,0 +1,17 @@
+themename = "High Contrast"
+
+gpu.setPaletteColor(colors.blue,0x0000ff)
+gpu.setPaletteColor(colors.red,0xff0000)
+gpu.setPaletteColor(colors.green,0x00ff00)
+gpu.setPaletteColor(colors.yellow,0xffff00)
+gpu.setPaletteColor(colors.magenta,0xff00ff)
+gpu.setPaletteColor(colors.cyan,0x0000ff)
+gpu.setPaletteColor(colors.black,0x000000)
+gpu.setPaletteColor(colors.gray,0x3c3c3c)
+gpu.setPaletteColor(colors.silver,0xa5a5a5)
+gpu.setPaletteColor(colors.white,0xffffff)
+
+topbarcolor = colors.blue
+textcolor = colors.yellow
+logcolor = colors.white
+bgcolor = colors.black
