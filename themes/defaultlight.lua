c69d9e95ddc220bf3a39c2835503be32f0a211ac
daysant <92995911+daysantace@users.noreply.github.com>
2023-02-28 12:39:47 +0000

    default light

---
+++ themes/defaultlight.lua
@@ -0,0 +1,17 @@
+themename = "Default Light"
+
+gpu.setPaletteColor(colors.blue,0x3349c0)
+gpu.setPaletteColor(colors.red,0xcc0000)
+gpu.setPaletteColor(colors.green,0x00db00)
+gpu.setPaletteColor(colors.yellow,0xffff00)
+gpu.setPaletteColor(colors.magenta,0xff24c0)
+gpu.setPaletteColor(colors.cyan,0x00b6ff)
+gpu.setPaletteColor(colors.black,0x000000)
+gpu.setPaletteColor(colors.gray,0x3c3c3c)
+gpu.setPaletteColor(colors.silver,0xa5a5a5)
+gpu.setPaletteColor(colors.white,0xffffff)
+
+topbarcolor = colors.cyan
+textcolor = colors.black
+logcolor = colors.silver
+bgcolor = colors.white
